274985b1b30f9e71a1c512df0e30ccd5986c4afc
orazovalibek <orazalike@gmail.com>
2020-10-31 12:42:22 -0400

    halloween party commit

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -3,6 +3,12 @@ public class HalloweenTest {
     public static void main(String[] args) {
         System.out.println("halloween party started");
         // this is comment just checking git pull
+
+        //  again changing
+        //  and again
+        //  and again
+        //  and again
+        //  and again
     }
 
 }
