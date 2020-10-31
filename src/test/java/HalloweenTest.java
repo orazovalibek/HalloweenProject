7f6ba3a6195c1d80fc1b72c0261a14f7276ba76b
orazovalibek <68873962+orazovalibek@users.noreply.github.com>
2020-10-31 12:34:46 -0400

    Update HalloweenTest.java

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -2,6 +2,7 @@ public class HalloweenTest {
 
     public static void main(String[] args) {
         System.out.println("halloween party started");
+        // this is comment just checking git pull
     }
 
 }
